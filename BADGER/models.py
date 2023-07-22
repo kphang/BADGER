18b5881d5fa1a40129393191e0d15f55b895903e
kphang <kevin.sl.phang@gmail.com>
2023-07-22 04:35:04 +0000

    move to autogen ids

--- BADGER/models.py
+++ BADGER/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
-from pydantic import HttpUrl, Json, validator
-from sqlmodel import SQLModel, Field, Column, JSON, create_engine
+from pydantic import HttpUrl, Json, validator, BaseModel
+from sqlmodel import SQLModel, Field, Column, JSON, create_engine, Session, Relationship
 from typing import Literal, List, Dict
 from uuid import UUID, uuid4
 
@@ -11,49 +11,35 @@ from httpx import Request, Response
 
 import logging 
 
+def BadgerItem(BaseModel):
+    id: int = Field(default=None, primary_key=True)
 
-class BadgerObj(SQLModel):
+class Job(BadgerItem,table=True):
     
-    id: int = Field(default=None, primary_key=True)        
+    #id: int = Field(default=None, primary_key=True)        
     created_at: datetime = Field(default=datetime.now())
     modified_at: datetime | None = None
-    is_deleted: bool = Field(default=False, index=True) # ! how to handle deleted items and archived items
-            
-    def updateobj(self) -> None:
-        self.modified_at = datetime.now()
-        return None
-
-    # ? what's the relationshp betwen last run and modified at
-    # ? does system updating the limits count as an update?
-    # ! this might not be particularly useful model since only jobs really get modified
-        # only jobs get deleted directly (everything else is cascaded from the job)
-
-class Job(BadgerObj,table=True):
-    
-    last_run: datetime | None = None
-    next_run: datetime | None = None
+    is_deleted: bool = Field(default=False, index=True)
     desc: str | None = None
-    #status: Literal["draft","active","inactive"] = Field(index=True) # ! sqlmodel issue with Literal (might have to use enum)
-    # batch config
-    batch_size: int
-    #asynclimits: List[AsyncLimiter] | None = None # ! should just be the args of AsyncLimiter?
-    n_concur: int = Field(default=5)
-    timeout: int = Field(default=60)
-    retries: int = Field(default=3)
-    reschedule: bool = Field(default=True) # ! need to make sure that period retry happens only once
-    # schedule config
-    #schedule: BackgroundScheduler # ! schedule items might be better as its own class and related
+    #status: Literal["draft","active","inactive"] = Field(index=True) # ? is this needed? sqlmodel issue with Literal (might have to use enum)    
+    #schedule: BackgroundScheduler # TODO: after implementing the scheduler's storage
     start_at: datetime # ?
-    repeat: bool = False
-    end_date: datetime | None = None
+    repeat: bool = False    
+    end_date: datetime | None = None    
+    last_run: datetime | None = None
+    next_run: datetime | None = None        
     
-    @validator("n_concur")
-    def checknconcur(cls,v) ->int:
-        return 1 if v < 1 else v # ? should it return error?
+    
+    
     
     @validator("start_at")
     def checkstartat(cls,v) -> datetime: # ? how to validate if the date is very close?
         return v
+
+    def create_reqspecs(self,file) -> None:
+        # iterate through file
+        
+        pass
             
     def run_job(self):
         
@@ -70,12 +56,33 @@ class Job(BadgerObj,table=True):
         
         pass
 
+    def updateobj(self) -> None:
+        # ? what's the relationshp betwen last run and modified at
+        # ? does system updating the limits count as an update?
+        self.modified_at = datetime.now()
+        return None
 
-class Batch(BadgerObj,table=True):
-    job_id: int = Field(index=True)
-    index: int = Field(index=True)
+class BatchConfig(SQLModel,table=True):
+    job_id: int = Field(foreign_key="job.id", primary_key=True)    
+    batch_size: int    
+    timeout: int = Field(default=60)
+    retries: int = Field(default=3)
+    reschedule: bool = Field(default=True) # ! need to make sure that period retry happens only once
+    #asynclimits: List[AsyncLimiter] | None = None # ! should just be the args of AsyncLimiter?
+    n_concur: int = Field(default=5)
+
+    @validator("n_concur")
+    def checknconcur(cls,v) ->int:
+        return 1 if v < 1 else v # ? should it return error?
+    
+    def create(self) -> None:
+        pass
+
+class Batch(BadgerItem,table=True):
+    job_id: int = Field(foreign_key="job.id")
+    index: int # use default_factory? can we create a general function that takes the object and searches for that object?
     run_at: datetime
-    reqspecs: List[int]           
+    reqspec_idxs: List["RequestSpec"] = Relationship(back_populates="requestspec.id",link_model="BatchReqSpec")# ! might be better to be a link table so we can use relationships          
     
     
     def build_requests() -> list:
@@ -123,8 +130,8 @@ class Batch(BadgerObj,table=True):
     
 class RequestSpec(SQLModel,table=True):
     
-    job_id: int = Field(primary_key=True)
-    index: int = Field(primary_key=True)
+    job_id: int = Field(foreign_key="job.id")
+    index: int # use default_factory?
     method: str
     url: HttpUrl    
     params: dict | None = Field(default=None, sa_column=Column(JSON))
@@ -137,12 +144,14 @@ class RequestSpec(SQLModel,table=True):
     # TODO: content        
     #status: Literal["Waiting","Retrying Later","Failed","Complete"]
     periodRetried: bool = False
+    batches: List[Batch] = Relationship(back_populates="")
     
     # ? validate that if params included it's not already present in url?
         
     
     # validate by creating an httpx request
-    
+    def create(self) -> None:
+        pass
     
     def build_request(self,client) -> Request:
         
@@ -150,13 +159,23 @@ class RequestSpec(SQLModel,table=True):
         
         return request
     
+    def get_response(self,final_only=False) -> list:
+        
+        
+        pass
+
+        
+class BatchReqSpec(SQLModel,table=True):
+    batch_id: int = Field(primary_key=True, foreign_key="batch.id")
+    reqspec_id: int = Field(primary_key=True, foreign_key="requestspec.id")
     
+class BadgerResponse(BadgerItem,table=True):
     
-class BadgerResponse(SQLModel,table=True):
-    
-    job_id: int = Field(primary_key=True)    
-    reqspec_index: int = Field(primary_key=True)    
-    index: int = Field(primary_key=True)    
+    reqspec_id: int = Field(foreign_key="requestspec.id")    
+    job_id: int = Field(foreign_key="job.id")
+    reqspec_index: int = Field(foreign_key="requestspec.index")        
+    batch_index: int = Field(foreign_key="batch.index")
+    index: int = Field(primary_key=True) # use default_factory?
     status_code: int
     delay_applied: int
     sent_at: datetime
@@ -164,38 +183,55 @@ class BadgerResponse(SQLModel,table=True):
     headers: dict = Field(sa_column=Column(JSON))
     text: str | None = None
     html: str | None = None
-    json: dict | None = Field(default=None, sa_column=Column(JSON))
+    body_json: dict | None = Field(default=None, sa_column=Column(JSON), alias="json")
     
+    def create(self) -> None:
+        pass
     
+    def next_index(self) -> int:
+        
+        pass
     # ? update reqspec if success?
 
-class Notification(BadgerObj,table=True):
+class Notification(SQLModel,table=True):
     
     # Notifications are associated with Jobs and Batches and are generated upon the end of every batch run or 
-    
+    test: str = Field()
         
+    id: int | None = Field(primary_key=True)
+    job_id: int = Field(foreign_key="job.id")
+    batch_index: int | None = Field(foreign_key="batch.index", default=None)        
+    flag: int = Field(index=True)
+    code: str
     
-    #type: Literal["Job","Batch"]
-    relatedId: int = Field(index=True)
-    code: str = Field(index=True)
-    
+    # Types of notifications                        
+            # Job finished
+            # Job finished (some incompletes)
+            # Job could not run on schedule - service was stopped            
+            # Batch finished
+            # Batch finished (some incompletes)            
+            
+    def create(self) -> None:
+        pass
     
     def describe(cls) -> str:
         
+        
         # translate code into a string description interpolated with relevant stats
-        # Types of notifications                        
-            # Batch success        
-            # Batch success (some incompletes)            
-            # Job completion
-            # Job completion (some incompletes)
-            # Job could not run on schedule - service was stopped
+        
         
         pass
         
     
     # def send_notification() # send to webhook
 
+def next_index(obj,field): # finish typing
+    with Session as session:
+        return session.select(obj).where(obj.field==)
+
+
 engine = create_engine(f"sqlite:///badgerdb.db", echo=True)
+
 
 def build_db():
     SQLModel.metadata.create_all(engine)
